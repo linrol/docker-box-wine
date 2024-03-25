4c3419a902f9b0e9a4f66eec28c22920681057d9
linrol <linrolmail@gmail.com>
2024-03-25 12:42:40 +0800

    add arm support

--- install-box.sh
+++ install-box.sh
@@ -14,7 +14,7 @@ if [ "$TARGETPLATFORM" == "linux/arm64" ]; then
    apt install box86-generic-arm:armhf -y
 
    # Add repo box64 to source list and install box64
-   wget https://ryanfortner.github.io/box64-debs/box64.list -O /etc/apt/sources.list.d/box64.list
-   wget -qO- https://ryanfortner.github.io/box64-debs/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
+   wget https://cdn05042023.gitlink.org.cn/shenmo7192/box64-debs/raw/branch/master/box64-CN.list -O /etc/apt/sources.list.d/box64.list
+   wget -qO- https://cdn05042023.gitlink.org.cn/shenmo7192/box64-debs/raw/branch/master/KEY.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/box64-debs-archive-keyring.gpg
    apt update && apt install box64-arm64 -y
 fi
